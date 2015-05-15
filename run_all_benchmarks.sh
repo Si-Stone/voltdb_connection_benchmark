98b73e5dae2c4ad17f5fadf5c7a19d24842f17f1
Si-Stone <Si-Stone@users.noreply.github.com>
2015-05-15 15:59:22 +1000

    use enum values for ClientType

--- run_all_benchmarks.sh
+++ run_all_benchmarks.sh
@@ -14,43 +14,43 @@ date
 
 # first try to get average connection/disconnection overhead as a percentage for each type
 # i.e. just connect, call proc once and disconnect
-number_of_connections=100000
+number_of_connections=10000
 number_of_proc_calls=1
 
 file=rpt_many_connections_1_proc_call_native_synch_client.txt
 sqlcmd < truncate.sql > ${file} 2>&1
-./run.sh native_synch_client ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
+./run.sh NATIVE_SYNCH_CLIENT ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
 sqlcmd < sanity_check_rowcount.sql >> ${file} 2>&1
 
 file=rpt_many_connections_1_proc_call_native_asynch_client.txt
 sqlcmd < truncate.sql > ${file} 2>&1
-./run.sh native_asynch_client ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
+./run.sh NATIVE_ASYNCH_CLIENT ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
 sqlcmd < sanity_check_rowcount.sql >> ${file} 2>&1
 
 file=rpt_many_connections_1_proc_call_jdbc_client.txt
 sqlcmd < truncate.sql > ${file} 2>&1
-./run.sh jdbc_client ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
+./run.sh JDBC_CLIENT ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
 sqlcmd < sanity_check_rowcount.sql >> ${file} 2>&1
 
 
 # next try to see how things change when connection is held open for many proc calls
 # i.e. connect, call procs many times and disconnect
 number_of_connections=1
-number_of_proc_calls=100000
+number_of_proc_calls=10000
 
 file=rpt_1_connection_many_proc_calls_native_synch_client.txt
 sqlcmd < truncate.sql > ${file} 2>&1
-./run.sh native_synch_client ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
+./run.sh NATIVE_SYNCH_CLIENT ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
 sqlcmd < sanity_check_rowcount.sql >> ${file} 2>&1
 
 file=rpt_1_connection_many_proc_calls_native_asynch_client.txt
 sqlcmd < truncate.sql > ${file} 2>&1
-./run.sh native_asynch_client ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
+./run.sh NATIVE_ASYNCH_CLIENT ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
 sqlcmd < sanity_check_rowcount.sql >> ${file} 2>&1
 
 file=rpt_1_connection_many_proc_call_jdbc_client.txt
 sqlcmd < truncate.sql > ${file} 2>&1
-./run.sh jdbc_client ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
+./run.sh JDBC_CLIENT ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
 sqlcmd < sanity_check_rowcount.sql >> ${file} 2>&1
 
 echo ended
