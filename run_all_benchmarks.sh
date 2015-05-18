1fc9c85bf67515c7bc245f0db61a767b152cb1da
Si_Stone <simon.stone@quantumit.com.au>
2015-05-18 12:17:51 +1000

    improve output from run_all_benchmarks.sh - echoes & dates

--- run_all_benchmarks.sh
+++ run_all_benchmarks.sh
@@ -17,6 +17,13 @@ date
 number_of_connections=1000
 number_of_proc_calls=1
 
+echo
+echo many connections in serial starts  at `date`
+echo
+echo number of connections is:  ${number_of_connections}
+echo number of proc calls is:   ${number_of_connections}
+echo
+
 file=rpt_many_connections_1_proc_call_native_synch_client.txt
 sqlcmd < truncate.sql > ${file} 2>&1
 ./run.sh native_synch_client ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
@@ -32,11 +39,21 @@ sqlcmd < truncate.sql > ${file} 2>&1
 ./run.sh jdbc_client ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
 sqlcmd < sanity_check_rowcount.sql >> ${file} 2>&1
 
+echo
+echo many connections in serial ends at `date`
+echo
 
 # next try to see how things change when connection is held open for many proc calls
 # i.e. connect, call procs many times and disconnect
 number_of_connections=1
-number_of_proc_calls=10000
+number_of_proc_calls=1000
+
+echo
+echo single connection many proc calls starts at `date`
+echo
+echo number of connections is:  ${number_of_connections}
+echo number of proc calls is:   ${number_of_connections}
+echo
 
 file=rpt_1_connection_many_proc_calls_native_synch_client.txt
 sqlcmd < truncate.sql > ${file} 2>&1
@@ -53,9 +70,24 @@ sqlcmd < truncate.sql > ${file} 2>&1
 ./run.sh jdbc_client ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
 sqlcmd < sanity_check_rowcount.sql >> ${file} 2>&1
 
+echo
+echo single connection many proc calls ends at `date`
+echo
+
 
 # now see how many we can run in parallel
 # i.e. parallel client connections as separate threads, each one of which calls proc once and disconnects
+
+number_of_connections=1000
+number_of_proc_calls=1
+
+echo
+echo many connections in parallel starts at `date`
+echo
+echo number of connections is:  ${number_of_connections}
+echo number of proc calls is:   ${number_of_connections}
+echo
+
 
 file=rpt_many_connections_1_proc_call_native_synch_client_parallel.txt
 sqlcmd < truncate.sql > ${file} 2>&1
@@ -67,12 +99,14 @@ sqlcmd < truncate.sql > ${file} 2>&1
 ./run.sh native_asynch_client_parallel ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
 sqlcmd < sanity_check_rowcount.sql >> ${file} 2>&1
 
-file=rpt_many_connections_1_proc_call_native_jdbc_client_parallel.txt
+file=rpt_many_connections_1_proc_call_jdbc_client_parallel.txt
 sqlcmd < truncate.sql > ${file} 2>&1
-./run.sh native_jdbc_client_parallel ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
+./run.sh jdbc_client_parallel ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
 sqlcmd < sanity_check_rowcount.sql >> ${file} 2>&1
 
-
+echo
+echo many connections in parallel ends at `date`
+echo
 
 echo ended
 date
