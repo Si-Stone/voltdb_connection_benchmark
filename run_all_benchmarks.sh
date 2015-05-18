26a3f474a85504aa1dfb60d9e24c903906cef368
Si_Stone <simon.stone@quantumit.com.au>
2015-05-18 11:52:38 +1000

    include 'parallel' benchmarks fully

--- run_all_benchmarks.sh
+++ run_all_benchmarks.sh
@@ -14,7 +14,7 @@ date
 
 # first try to get average connection/disconnection overhead as a percentage for each type
 # i.e. just connect, call proc once and disconnect
-number_of_connections=10000
+number_of_connections=1000
 number_of_proc_calls=1
 
 file=rpt_many_connections_1_proc_call_native_synch_client.txt
@@ -52,6 +52,27 @@ file=rpt_1_connection_many_proc_call_jdbc_client.txt
 sqlcmd < truncate.sql > ${file} 2>&1
 ./run.sh jdbc_client ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
 sqlcmd < sanity_check_rowcount.sql >> ${file} 2>&1
+
+
+# now see how many we can run in parallel
+# i.e. parallel client connections as separate threads, each one of which calls proc once and disconnects
+
+file=rpt_many_connections_1_proc_call_native_synch_client_parallel.txt
+sqlcmd < truncate.sql > ${file} 2>&1
+./run.sh native_synch_client_parallel ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
+sqlcmd < sanity_check_rowcount.sql >> ${file} 2>&1
+
+file=rpt_many_connections_1_proc_call_native_asynch_client_parallel.txt
+sqlcmd < truncate.sql > ${file} 2>&1
+./run.sh native_asynch_client_parallel ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
+sqlcmd < sanity_check_rowcount.sql >> ${file} 2>&1
+
+file=rpt_many_connections_1_proc_call_native_jdbc_client_parallel.txt
+sqlcmd < truncate.sql > ${file} 2>&1
+./run.sh native_jdbc_client_parallel ${number_of_connections} ${number_of_proc_calls} >> ${file} 2>&1
+sqlcmd < sanity_check_rowcount.sql >> ${file} 2>&1
+
+
 
 echo ended
 date
